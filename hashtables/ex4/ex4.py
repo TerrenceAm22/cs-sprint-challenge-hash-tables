38567c9e5d7c6a7a57491d4bee1e5693aa22007b
TerrenceAm22 <tamalone87@gmail.com>
2020-10-02 12:26:34 -0400

    almost done

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,8 +1,21 @@
 def has_negatives(a):
     """
-    YOUR CODE HERE
+    
     """
     # Your code here
+    result = []
+
+    posNumber = {}
+    negNUmber = {}
+    for number in a:
+        if number > 0:
+            posNumber[number] = number
+        else:
+            negNUmber[number] = number
+
+    for number in posNumber.keys():
+        if negNUmber.get(number * (-1)):
+            result.append(number)
 
     return result
 
